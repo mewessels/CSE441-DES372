05d20ef2344eca761d53827e59324aedfebf1cb4
Justin Tran <tranjustins@gmail.com>
2018-05-15 21:21:21 -0700

    added comments

--- website/responses.js
+++ website/responses.js
@@ -1,4 +1,5 @@
 function createArray() {
+  // creates responses array in localStorage if it doesn't already exist
   if (!localStorage.getItem("responses")) {
     localStorage.setItem("responses", JSON.stringify([]));
   }
@@ -7,6 +8,6 @@ function createArray() {
 function submitResponse() {
   let responses = JSON.parse(localStorage.getItem("responses"));
   responses.push(document.getElementById("response").value);
-  localStorage.setItem("responses", JSON.stringify(responses));
+  localStorage.setItem("responses", JSON.stringify(responses)); // localStorage only stores strings so we store the array in JSON format
   console.log(responses);
 }
